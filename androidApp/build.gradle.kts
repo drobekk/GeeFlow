0ce0726fb8ee758cb2669d3c5be59588dcfa47db
drobekk <drobek.krzysztof@gmail.com>
2026-09-18 20:33:04 +0200

    Include native debug symbol table in release app bundle

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -48,6 +48,9 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
             )
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
+            }
         }
         debug {
             signingConfig = signingConfigs.getByName("debug")
