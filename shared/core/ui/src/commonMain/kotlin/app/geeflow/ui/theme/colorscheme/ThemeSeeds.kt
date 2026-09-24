cea27116c9acaef246ef887e8be8c9c7a7a97886
drobekk <drobek.krzysztof@gmail.com>
2026-09-24 19:23:45 +0200

    Update default theme color

--- shared/core/ui/src/commonMain/kotlin/app/geeflow/ui/theme/colorscheme/ThemeSeeds.kt
+++ shared/core/ui/src/commonMain/kotlin/app/geeflow/ui/theme/colorscheme/ThemeSeeds.kt
@@ -2,7 +2,7 @@ package app.geeflow.ui.theme.colorscheme
 
 import androidx.compose.ui.graphics.Color
 
-val EspressoSeed = Color(0xFF7F5610)
+val EspressoSeed = Color(0xFF2D1910)
 val SapphireSeed = Color(0xFF0F4FB4)
 val EmeraldSeed = Color(0xFF1B6B45)
 val RoseSeed = Color(0xFF8C4A60)
